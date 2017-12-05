bf85aac7219d869f44d3de9c5af05f63f808cbae
paul123k <paul123k@naver.com>
2017-12-05 16:25:08 +0900

    2017/12/05
    2017/12/5

--- C_Sharp/C_Sharp/diagramForm.Designer.cs
+++ C_Sharp/C_Sharp/diagramForm.Designer.cs
@@ -40,20 +40,22 @@
             this.weak_entity = new System.Windows.Forms.ToolStripButton();
             this.relation = new System.Windows.Forms.ToolStripButton();
             this.weak_relation = new System.Windows.Forms.ToolStripButton();
-            this.er_table = new System.Windows.Forms.ToolStripButton();
             this.line = new System.Windows.Forms.ToolStripSplitButton();
             this.mainPanel = new System.Windows.Forms.Panel();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
             this.menuStrip1.SuspendLayout();
             this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
             // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.fileToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1184, 24);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(7, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(1351, 28);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -63,23 +65,24 @@
             this.saveToolStripMenuItem,
             this.loadToolStripMenuItem});
             this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(44, 24);
             this.fileToolStripMenuItem.Text = "File";
             // 
             // saveToolStripMenuItem
             // 
             this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
-            this.saveToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(117, 26);
             this.saveToolStripMenuItem.Text = "Save";
             // 
             // loadToolStripMenuItem
             // 
             this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
-            this.loadToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.loadToolStripMenuItem.Size = new System.Drawing.Size(117, 26);
             this.loadToolStripMenuItem.Text = "Load";
             // 
             // toolStrip1
             // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.attribute,
             this.multi_attribute,
@@ -87,23 +90,23 @@
             this.weak_entity,
             this.relation,
             this.weak_relation,
-            this.er_table,
-            this.line});
-            this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+            this.line,
+            this.toolStripButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 28);
             this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(1184, 71);
+            this.toolStrip1.Size = new System.Drawing.Size(1351, 75);
             this.toolStrip1.TabIndex = 1;
             this.toolStrip1.Text = "toolStrip1";
             // 
             // attribute
             // 
-            this.attribute.Font = new System.Drawing.Font("나눔바른고딕", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.attribute.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.attribute.Image = global::C_Sharp.Properties.Resources.relationship;
             this.attribute.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
             this.attribute.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
             this.attribute.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.attribute.Name = "attribute";
-            this.attribute.Size = new System.Drawing.Size(59, 68);
+            this.attribute.Size = new System.Drawing.Size(64, 72);
             this.attribute.Text = "attribute";
             this.attribute.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.attribute.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
@@ -111,13 +114,13 @@
             // 
             // multi_attribute
             // 
-            this.multi_attribute.Font = new System.Drawing.Font("나눔바른고딕", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.multi_attribute.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.multi_attribute.Image = global::C_Sharp.Properties.Resources.weakAttribute;
             this.multi_attribute.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.multi_attribute.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
             this.multi_attribute.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.multi_attribute.Name = "multi_attribute";
-            this.multi_attribute.Size = new System.Drawing.Size(90, 68);
+            this.multi_attribute.Size = new System.Drawing.Size(99, 72);
             this.multi_attribute.Text = "multi attribute";
             this.multi_attribute.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.multi_attribute.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
@@ -126,101 +129,98 @@
             // 
             // entity
             // 
-            this.entity.Font = new System.Drawing.Font("나눔바른고딕", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.entity.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.entity.Image = global::C_Sharp.Properties.Resources.Entity;
             this.entity.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
             this.entity.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.entity.Name = "entity";
-            this.entity.Size = new System.Drawing.Size(52, 68);
+            this.entity.Size = new System.Drawing.Size(52, 72);
             this.entity.Text = "entity";
             this.entity.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             this.entity.Click += new System.EventHandler(this.entity_Click);
             // 
             // weak_entity
             // 
-            this.weak_entity.Font = new System.Drawing.Font("나눔바른고딕", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.weak_entity.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.weak_entity.Image = global::C_Sharp.Properties.Resources.weakEntity;
             this.weak_entity.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
             this.weak_entity.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.weak_entity.Name = "weak_entity";
-            this.weak_entity.Size = new System.Drawing.Size(74, 68);
+            this.weak_entity.Size = new System.Drawing.Size(85, 72);
             this.weak_entity.Text = "weak entity";
             this.weak_entity.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             this.weak_entity.Click += new System.EventHandler(this.weak_entity_Click);
             // 
             // relation
             // 
-            this.relation.Font = new System.Drawing.Font("나눔바른고딕", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.relation.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.relation.Image = ((System.Drawing.Image)(resources.GetObject("relation.Image")));
             this.relation.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
             this.relation.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.relation.Name = "relation";
-            this.relation.Size = new System.Drawing.Size(54, 68);
+            this.relation.Size = new System.Drawing.Size(60, 72);
             this.relation.Text = "relation";
             this.relation.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
-            this.relation.Click += new System.EventHandler(this.relation_Click);
+            this.relation.Click += new System.EventHandler(this.relationship_Click);
             // 
             // weak_relation
             // 
-            this.weak_relation.Font = new System.Drawing.Font("나눔바른고딕", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.weak_relation.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.weak_relation.Image = ((System.Drawing.Image)(resources.GetObject("weak_relation.Image")));
             this.weak_relation.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
             this.weak_relation.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.weak_relation.Name = "weak_relation";
-            this.weak_relation.Size = new System.Drawing.Size(86, 68);
+            this.weak_relation.Size = new System.Drawing.Size(99, 72);
             this.weak_relation.Text = "weak relation";
             this.weak_relation.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
-            this.weak_relation.Click += new System.EventHandler(this.weak_relation_Click);
-            // 
-            // er_table
-            // 
-            this.er_table.Font = new System.Drawing.Font("나눔바른고딕", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.er_table.Image = ((System.Drawing.Image)(resources.GetObject("er_table.Image")));
-            this.er_table.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
-            this.er_table.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.er_table.Name = "er_table";
-            this.er_table.Size = new System.Drawing.Size(56, 68);
-            this.er_table.Text = "ER table";
-            this.er_table.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
-            this.er_table.ToolTipText = "er_table";
-            this.er_table.Click += new System.EventHandler(this.er_table_Click);
+            this.weak_relation.Click += new System.EventHandler(this.weak_relationship_Click);
             // 
             // line
             // 
-            this.line.Font = new System.Drawing.Font("나눔바른고딕", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.line.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.line.Image = ((System.Drawing.Image)(resources.GetObject("line.Image")));
             this.line.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
             this.line.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.line.Name = "line";
-            this.line.Size = new System.Drawing.Size(66, 68);
+            this.line.Size = new System.Drawing.Size(69, 72);
             this.line.Text = "line";
             this.line.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             // 
             // mainPanel
             // 
-            this.mainPanel.Location = new System.Drawing.Point(0, 98);
+            this.mainPanel.Location = new System.Drawing.Point(0, 122);
+            this.mainPanel.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.mainPanel.Name = "mainPanel";
-            this.mainPanel.Size = new System.Drawing.Size(1200, 702);
+            this.mainPanel.Size = new System.Drawing.Size(1371, 878);
             this.mainPanel.TabIndex = 2;
             this.mainPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.mainPanel_Paint);
             this.mainPanel.MouseClick += new System.Windows.Forms.MouseEventHandler(this.mainPanel_MouseClick);
             // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(24, 72);
+            this.toolStripButton1.Text = "toolStripButton1";
+            // 
             // diagramForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1184, 761);
+            this.ClientSize = new System.Drawing.Size(1351, 941);
             this.Controls.Add(this.mainPanel);
             this.Controls.Add(this.toolStrip1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.MaximizeBox = false;
-            this.MaximumSize = new System.Drawing.Size(1200, 800);
+            this.MaximumSize = new System.Drawing.Size(1369, 988);
             this.MinimizeBox = false;
-            this.MinimumSize = new System.Drawing.Size(1200, 800);
+            this.MinimumSize = new System.Drawing.Size(1369, 988);
             this.Name = "diagramForm";
             this.Text = "diagramForm";
-       
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.toolStrip1.ResumeLayout(false);
@@ -243,8 +243,8 @@
         private System.Windows.Forms.ToolStripButton weak_entity;
         private System.Windows.Forms.ToolStripButton relation;
         private System.Windows.Forms.ToolStripButton weak_relation;
-        private System.Windows.Forms.ToolStripButton er_table;
         private System.Windows.Forms.ToolStripSplitButton line;
         private System.Windows.Forms.Panel mainPanel;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
     }
 }
--- C_Sharp/C_Sharp/diagramForm.cs
+++ C_Sharp/C_Sharp/diagramForm.cs
@@ -50,12 +50,12 @@ namespace C_Sharp
          
         }
 
-        private void relation_Click(object sender, EventArgs e)
+        private void relationship_Click(object sender, EventArgs e)
         {
             flag[4] = true;
         }
 
-        private void weak_relation_Click(object sender, EventArgs e)
+        private void weak_relationship_Click(object sender, EventArgs e)
         {
             flag[5] = true;
         }
@@ -63,7 +63,7 @@ namespace C_Sharp
         private void er_table_Click(object sender, EventArgs e)
         {
             //er_table 은 flag 6과 7을 같이쓴다!!!
-            flag[6] = true;
+            //flag[6] = true;
            
         }
 
@@ -81,10 +81,21 @@ namespace C_Sharp
                 else if (da.type == 'E') e.Graphics.DrawEllipse(new Pen(da.color), da.rect); //attibute
                 else if (da.type == 'O') e.Graphics.DrawEllipse(new Pen(da.color), da.rect);  //multi attibute
 
-               
+
                 else if (da.type == 'B') e.Graphics.DrawEllipse(new Pen(da.color), da.rect);  //multi attibute
 
-             
+                else if (da.type == 'S') {
+                    Point[] PointArray = { new Point((da.rect.Left+da.rect.Right)/2, da.rect.Top), new Point(da.rect.Right, (da.rect.Top+da.rect.Bottom)/2), new Point((da.rect.Left+da.rect.Right)/2, da.rect.Bottom), new Point(da.rect.Left, (da.rect.Top + da.rect.Bottom)/2)};
+          
+                    e.Graphics.DrawPolygon(new Pen(da.color), PointArray);
+                }
+
+                else if (da.type == 'X')
+                {
+                    Point[] PointArray = { new Point((da.rect.Left + da.rect.Right) / 2, da.rect.Top), new Point(da.rect.Right, (da.rect.Top + da.rect.Bottom) / 2), new Point((da.rect.Left + da.rect.Right) / 2, da.rect.Bottom), new Point(da.rect.Left, (da.rect.Top + da.rect.Bottom) / 2) };
+
+                    e.Graphics.DrawPolygon(new Pen(da.color), PointArray);
+                }
 
 
             }
@@ -109,6 +120,8 @@ namespace C_Sharp
             if (flag[0])
             {
                 Point p = mainPanel.PointToClient(Cursor.Position);
+                p.X = p.X - 44;
+                p.Y = p.Y - 44;
                 actions.Add(new DrawAction('E', new Rectangle(p.X, p.Y, 88, 88), Color.Black));
                 mainPanel.Invalidate();  // this triggers the Paint event!
                 flag[0] = false;
@@ -116,14 +129,18 @@ namespace C_Sharp
             else if(flag[1])
             {
                 Point p = mainPanel.PointToClient(Cursor.Position);
+                p.X = p.X - 44;
+                p.Y = p.Y - 44;
                 actions.Add(new DrawAction('O', new Rectangle(p.X, p.Y, 88, 88), Color.Black));
-                actions.Add(new DrawAction('O', new Rectangle(p.X+10, p.Y+10, 66, 66), Color.Black));
+                actions.Add(new DrawAction('O', new Rectangle(p.X+5, p.Y+5, 78, 78), Color.Black));
                 mainPanel.Invalidate();  // this triggers the Paint event!
                 flag[1] = false;
             }
             else if (flag[2])
             {
                 Point p = mainPanel.PointToClient(Cursor.Position);
+                p.X = p.X - 44;
+                p.Y = p.Y - 44;
                 actions.Add(new DrawAction('R', new Rectangle(p.X, p.Y, 88, 88), Color.Black));
                 mainPanel.Invalidate();  // this triggers the Paint event!
                 flag[2] = false;
@@ -131,27 +148,38 @@ namespace C_Sharp
             else if (flag[3])
             {
                 Point p = mainPanel.PointToClient(Cursor.Position);
+                p.X = p.X - 44;
+                p.Y = p.Y - 44;
                 actions.Add(new DrawAction('W', new Rectangle(p.X, p.Y, 88, 88), Color.Black));
-                actions.Add(new DrawAction('W', new Rectangle(p.X+10, p.Y+10, 66, 66), Color.Black));
+                actions.Add(new DrawAction('W', new Rectangle(p.X+5, p.Y+5, 78, 78), Color.Black));
                 mainPanel.Invalidate();  // this triggers the Paint event!
                 flag[3] = false;
             }
             else if (flag[4])
             {
-               
+                Point p = mainPanel.PointToClient(Cursor.Position);
+                p.X = p.X - 44;
+                p.Y = p.Y - 44;
+                actions.Add(new DrawAction('S', new Rectangle(p.X, p.Y, 124, 124), Color.Black));
+                mainPanel.Invalidate();  // this triggers the Paint event!
                 flag[4] = false;
             }
             else if (flag[5])
             {
-
+                Point p = mainPanel.PointToClient(Cursor.Position);
+                p.X = p.X - 44;
+                p.Y = p.Y - 44;
+                actions.Add(new DrawAction('X', new Rectangle(p.X, p.Y, 124, 124), Color.Black));
+                actions.Add(new DrawAction('X', new Rectangle(p.X + 5, p.Y + 5, 114, 114), Color.Black));
+                mainPanel.Invalidate();  // this triggers the Paint event!
                 flag[5] = false;
             }
-            else if (flag[6])
-            {
+            //else if (flag[6])
+            //{
 
-                di.Add(new DrawImage('Z',er_table.Image));
-                flag[7] = true;
-            }
+            //    di.Add(new DrawImage('Z',er_table.Image));
+            //    flag[7] = true;
+            //}
            
 
         }
